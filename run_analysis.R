cb4b03228528f5e314f3046c50b82a99b0e3e466
Kysamah <85692192+Kysamah@users.noreply.github.com>
2021-06-16 09:48:12 +0200

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+library(downloader)
+library(plyr)
+library(knitr)
+
+if(!file.exists("./projectData")){
+  dir.create("./projectData")
+}
+Url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+if(!file.exists("./projectData/project_Dataset.zip")){
+  download.file(Url,destfile="./projectData/project_Dataset.zip",mode = "wb")
+}
+
+if(!file.exists("./projectData/UCI HAR Dataset")){
+  unzip(zipfile="./projectData/project_Dataset.zip",exdir="./projectData")
+}
+
+path <- file.path("./projectData" , "UCI HAR Dataset")
+files<-list.files(path, recursive=TRUE)
+
+ActivityTest  <- read.table(file.path(path, "test" , "Y_test.txt" ),header = FALSE)
+ActivityTrain <- read.table(file.path(path, "train", "Y_train.txt"),header = FALSE)
+
+SubjectTrain <- read.table(file.path(path, "train", "subject_train.txt"),header = FALSE)
+SubjectTest  <- read.table(file.path(path, "test" , "subject_test.txt"),header = FALSE)
+
+FeaturesTest  <- read.table(file.path(path, "test" , "X_test.txt" ),header = FALSE)
+FeaturesTrain <- read.table(file.path(path, "train", "X_train.txt"),header = FALSE)
+
+dataSubject <- rbind(SubjectTrain, SubjectTest)
+dataActivity<- rbind(ActivityTrain, ActivityTest)
+dataFeatures<- rbind(FeaturesTrain, FeaturesTest)
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+activityLabels <- read.table(file.path(path, "activity_labels.txt"),header = FALSE)
+Data$activity<-factor(Data$activity,labels=activityLabels[,2])
+head(Data$activity,30)
+
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+
+newData<-aggregate(. ~subject + activity, Data, mean)
+newData<-newData[order(newData$subject,newData$activity),]
+write.table(newData, file = "tidydata.txt",row.name=FALSE,quote = FALSE, sep = '\t')
